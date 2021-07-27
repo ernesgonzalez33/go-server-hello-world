331796864c091af0cd8d256ce68fcd4f4ef7dd14
Ernesto González <ernesgonzalez.33.egc@icloud.com>
2021-07-27 16:28:34 +0200

    Now the message is different depending on the connection

--- server.go
+++ server.go
@@ -2,10 +2,18 @@ package main
 
 import (
 	"crypto/tls"
-	"fmt"
+	"io"
 	"log"
 	"net/http"
 )
+
+func httpHandler(w http.ResponseWriter, req *http.Request) {
+	io.WriteString(w, "Hello, world from HTTP!\n")
+}
+
+func httpsHandler(w http.ResponseWriter, req *http.Request) {
+	io.WriteString(w, "Hello, world from HTTPS!\n")
+}
 
 func main() {
 
@@ -15,7 +23,7 @@ func main() {
 	// create a custom server with `TLSConfig`
 	s := &http.Server{
 		Addr:    ":9000",
-		Handler: nil, // use `http.DefaultServeMux`
+		Handler: http.HandlerFunc(httpsHandler), // use `http.DefaultServeMux`
 		TLSConfig: &tls.Config{
 			Certificates: []tls.Certificate{cert},
 		},
@@ -23,13 +31,13 @@ func main() {
 
 	s2 := &http.Server{
 		Addr:    ":8000",
-		Handler: nil, // use `http.DefaultServeMux`
+		Handler: http.HandlerFunc(httpHandler), // use `http.DefaultServeMux`
 	}
 
-	// handle `/` route
-	http.HandleFunc("/", func(res http.ResponseWriter, req *http.Request) {
-		fmt.Fprint(res, "Hello World!")
-	})
+	// // handle `/` route
+	// http.HandleFunc("/", func(res http.ResponseWriter, req *http.Request) {
+	// 	fmt.Fprint(res, "Hello World!")
+	// })
 
 	// run https server on port "9000"
 	go func() {
